c5b8b579248b4fe84ccc93a12a4d0efa58361d06
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-02-15 07:34:40 -0200

    setting the heroku port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+var port = process.env.PORT || 8080;
 
 
 // respond with "hello world" when a GET request is made to the homepage
